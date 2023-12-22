c9282dbcd8f2ea1167adaba3b1b1d133edb0cfdf
Tung Nguyen <ngtientung98@gmail.com>
2023-12-22 08:04:24 +0700

    Enums & Subtypes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
-console.log([1, 2, 3].map((i) => i + 1));
+// console.log([1, 2, 3].map((i) => i + 1));
 
-import * as fs from 'fs';
-import * as path from 'node:path';
+// import * as fs from 'fs';
+// import * as path from 'node:path';
 
-fs.readFile(path.resolve('projects/lines'), 'utf-8', (err, data) => {
-  if (err) throw err;
-  data.split('\n').forEach((line, i) => (i % 2 == 0 ? console.log(line) : null));
-});
+// fs.readFile(path.resolve('projects/lines'), 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   data.split('\n').forEach((line, i) => (i % 2 == 0 ? console.log(line) : null));
+// });
+
+type Custom = {
+  age: number;
+  name: string;
+};
+
+type Item = number | string | Custom;
+
+const append = (items: Item[]) => items.push('Hello Fem!');
+
+const myItems: Item[] = [12, 'hehe', { age: 12, name: 'Test' }];
+
+append(myItems);
+const numbers: number[] = [1, 2, 3, 4];
+append(numbers);
+// WTF ?
+console.log(numbers);
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,16 @@
-// fn main() {
-//   let array: Vec<isize> = vec![1,2,3].iter().map(|x| x+1).collect();
-//   print!("{:?}", array);
-// }
+enum Colors {
+    Red,
+    Green,
+    Blue
+}
 
-// use std::collections::{HashSet, HashMap};
-
-// fn main(){
-//   let data = vec![1,2,3];
-//   let mut vector = data.iter().map(|x: &i32| x+1);
-//   let mut new_vector = vec![];
-
-//   while let Some(x) = vector.next() {
-//       new_vector.push(x);
-//   }
-
-//   println!("{:?}", new_vector);
-
-//   let foo: String = vec!["I", "am", "a", "string"].into_iter().collect();
-//   println!("{0}", foo);
-  
-//   let hs: HashSet<isize> = vec![1,2,3].into_iter().collect();
-//   println!("{:?}", hs);
-
-//   vec![1,2,5,9,4].into_iter().skip(2).take_while(|&x| x > 4).for_each(|x| println!("{}", x));
-
-//   let what_about_this = vec![1,2,3].iter().filter(|&x| x % 2 == 0).count();
-
-//   let hm: HashMap<&str, usize> = vec!["this", "is", "a", "test"].into_iter().enumerate().map(|(index, item)| (item, index)).collect();
-//   println!("{:?}", what_about_this);
-//   println!("{:?}", hm);
-// }
-
-fn main(){
-  let file = std::fs::read_to_string("projects/lines").unwrap();
-  file.lines().enumerate().for_each(|(index, line)| if index % 2 == 0  {println!("{}", line)} else {});
+fn print_color(color: Colors) -> () {
+  match color {
+    Colors::Red => println!("red"),
+    Colors::Green => println!("green"),
+    Colors::Blue => println!("blue"),
+};
+}
+fn main() {
+    print_color(Colors::Green);
 }
